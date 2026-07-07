a2680b625b7f7f8199816073a853a5aa77dc68e0
Dilki bhanusha <mldbsithari@students.nsbm.ac.lk>
2026-07-08 00:26:31 +0530

    Remove AttendanceRepository interface

--- backend/src/main/java/com/uni/universityERP/repository/AttendanceRepository.java
+++ backend/src/main/java/com/uni/universityERP/repository/AttendanceRepository.java
@@ -1,9 +1,2 @@
 package src.main.java.com.uni.universityERP.repository;
-
-import src.main.java.com.uni.universityERP.model.Attendance;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AttendanceRepository extends JpaRepository<Attendance, Long> {
-}
+ 
