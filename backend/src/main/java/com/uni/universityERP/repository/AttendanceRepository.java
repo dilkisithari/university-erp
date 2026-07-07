39c28614d50b4ae9de2afd3597d6cc4c97fff577
dilkisithari <dilkibhanusha02@gmail.com>
2026-07-07 23:54:04 +0530

    Added full Attendance Management Module

--- backend/src/main/java/com/uni/universityERP/repository/AttendanceRepository.java
+++ backend/src/main/java/com/uni/universityERP/repository/AttendanceRepository.java
@@ -1,4 +1,9 @@
 package src.main.java.com.uni.universityERP.repository;
 
-public class AttendanceRepository {
+import src.main.java.com.uni.universityERP.model.Attendance;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AttendanceRepository extends JpaRepository<Attendance, Long> {
 }
