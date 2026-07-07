9669d7612cf62377b5067835bf5c91ff912e8af3
Dilki bhanusha <mldbsithari@students.nsbm.ac.lk>
2026-07-08 00:27:34 +0530

    Remove AttendanceService implementation
    Removed AttendanceService methods and dependencies.

--- backend/src/main/java/com/uni/universityERP/service/AttendanceService.java
+++ backend/src/main/java/com/uni/universityERP/service/AttendanceService.java
@@ -1,32 +1,5 @@
 package src.main.java.com.uni.universityERP.service;
 
-import src.main.java.com.uni.universityERP.model.Attendance;
-import src.main.java.com.uni.universityERP.repository.AttendanceRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-import java.util.List;
-import java.util.Optional;
-
-@Service
 public class AttendanceService {
 
-    @Autowired
-    private AttendanceRepository attendanceRepository;
-
-    public List<Attendance> getAllAttendance() {
-        return attendanceRepository.findAll();
-    }
-
-    public Optional<Attendance> getAttendanceById(Long id) {
-        return attendanceRepository.findById(id);
-    }
-
-    public Attendance saveAttendance(Attendance attendance) {
-        return attendanceRepository.save(attendance);
-    }
-
-    public void deleteAttendance(Long id) {
-        attendanceRepository.deleteById(id);
-    }
 }
