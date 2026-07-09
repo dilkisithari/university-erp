c815f1433ccf0b1b9567fb3a28fe8eef5d8020c9
dilkisithari <dilkibhanusha02@gmail.com>
2026-07-09 14:14:06 +0530

    Added full Exam Management Module

--- backend/src/main/java/com/uni/universityERP/service/ExamService.java
+++ backend/src/main/java/com/uni/universityERP/service/ExamService.java
@@ -1,4 +1,32 @@
 package src.main.java.com.uni.universityERP.service;
 
+import src.main.java.com.uni.universityERP.model.Exam;
+import src.main.java.com.uni.universityERP.repository.ExamRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class ExamService {
+
+    @Autowired
+    private ExamRepository examRepository;
+
+    public List<Exam> getAllExams() {
+        return examRepository.findAll();
+    }
+
+    public Optional<Exam> getExamById(Long id) {
+        return examRepository.findById(id);
+    }
+
+    public Exam saveExam(Exam exam) {
+        return examRepository.save(exam);
+    }
+
+    public void deleteExam(Long id) {
+        examRepository.deleteById(id);
+    }
 }
