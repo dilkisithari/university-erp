f176f8c67a686a3abc56f0d603611ddf845e230d
Dilki bhanusha <mldbsithari@students.nsbm.ac.lk>
2026-07-09 14:26:15 +0530

    Delete ExamRepository interface
    Removed the ExamRepository interface and its annotations.

--- backend/src/main/java/com/uni/universityERP/repository/ExamRepository.java
+++ backend/src/main/java/com/uni/universityERP/repository/ExamRepository.java
@@ -1,9 +1,5 @@
 package src.main.java.com.uni.universityERP.repository;
 
-import src.main.java.com.uni.universityERP.model.Exam;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
-public interface ExamRepository extends JpaRepository<Exam, Long> {
+public ExamRepository  {
 }
