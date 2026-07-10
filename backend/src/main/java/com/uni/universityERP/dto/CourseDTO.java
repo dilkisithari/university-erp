719945a5a762a92e191283729656199583c4e53e
dilkisithari <dilkibhanusha02@gmail.com>
2026-07-10 14:46:34 +0530

    Added CourseDTO

---
+++ backend/src/main/java/com/uni/universityERP/dto/CourseDTO.java
@@ -0,0 +1,15 @@
+package src.main.java.com.uni.universityERP.dto;
+
+import lombok.Data;
+
+@Data
+public class CourseDTO {
+
+    private Long id;
+    private String courseName;
+    private String courseCode;
+    private String description;
+    private Integer credits;
+    private String department;
+    private Boolean isActive;
+}
