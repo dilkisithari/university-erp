0c339391b2ed064b1f666d0efd562c50bc93993f
Eesha2263 <eesha.bhashini2023@gmail.com>
2026-07-10 13:09:00 +0530

    Added full Exam Management Module

--- backend/src/main/java/com/uni/universityERP/controller/ExamController.java
+++ backend/src/main/java/com/uni/universityERP/controller/ExamController.java
@@ -1,4 +1,34 @@
 package src.main.java.com.uni.universityERP.controller;
 
+import src.main.java.com.uni.universityERP.model.Exam;
+import src.main.java.com.uni.universityERP.service.ExamService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping("/exams")
 public class ExamController {
+
+    @Autowired
+    private ExamService examService;
+
+    @GetMapping
+    public String listExams(Model model) {
+        model.addAttribute("exams", examService.getAllExams());
+        return "exams/list";
+    }
+
+    @GetMapping("/new")
+    public String newExamForm(Model model) {
+        model.addAttribute("exam", new Exam());
+        return "exams/form";
+    }
+
+    @PostMapping
+    public String saveExam(@ModelAttribute Exam exam) {
+        examService.saveExam(exam);
+        return "redirect:/exams";
+    }
 }
--- backend/src/main/java/com/uni/universityERP/repository/ExamRepository.java
+++ backend/src/main/java/com/uni/universityERP/repository/ExamRepository.java
@@ -1,4 +1,9 @@
 package src.main.java.com.uni.universityERP.repository;
 
-public class ExamRepository {
+import src.main.java.com.uni.universityERP.model.Exam;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ExamRepository extends JpaRepository<Exam, Long> {
 }
