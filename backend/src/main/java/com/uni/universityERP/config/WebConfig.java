605956142e6043c2bd1539d5e1f9810d0f716f3f
dilkisithari <dilkibhanusha02@gmail.com>
2026-07-10 14:38:33 +0530

    Added config files and fixed modules

--- backend/src/main/java/com/uni/universityERP/config/WebConfig.java
+++ backend/src/main/java/com/uni/universityERP/config/WebConfig.java
@@ -1,4 +1,8 @@
 package src.main.java.com.uni.universityERP.config;
 
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class WebConfig {
+    // Basic config for now
 }
